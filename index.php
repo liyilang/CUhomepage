ff0250683fd0ed968e82d414fc5cfc66a564516b
Seth Lee <Seth Lee>
2016-08-29 12:12:19 -0400

    create

---
+++ index.php
@@ -0,0 +1,246 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Yilang Li(Seth)</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="pages/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+	
+	 <link href="pages/css/style.css" rel="stylesheet">
+	
+    <!-- Theme CSS -->
+    <link href="pages/css/freelancer.min.css" rel="stylesheet">
+	<link href="pages/css/full-slider.css" rel="stylesheet">
+	
+    <!-- Custom Fonts -->
+    <link href="pages/vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body id="page-top" class="index">
+
+    <!-- Navigation -->
+    <nav id="mainNav" class="navbar navbar-default navbar-fixed-top navbar-custom">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header page-scroll">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span> Menu <i class="fa fa-bars"></i>
+                </button>
+                <div class="navbar-brand" >
+					<img class="img-responsive" src="pages/img/tiger.png" alt="" style="display: inline;width: 35px;">
+					<a href="#myCarousel" style="text-transform: none;color:#fff;text-decoration:none">Yilang Li (Seth) </a>
+				</div>
+            </div>
+			
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+					<li class="page-scroll">
+                        <a href="#myCarousel">Cover</a>
+                    </li>
+                    <li class="page-scroll">
+                        <a href="#portfolio">Courses</a>
+                    </li>
+                    <li class="page-scroll">
+                        <a href="#contact">Contact</a>
+                    </li>
+					<li class="page-scroll">
+                        <a href="https://calendar.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;
+							src=yilangl%40g.clemson.edu&amp;color=%231B887A&amp;
+							src=g.clemson.edu_q35l7u7pc7uec9bo863163fmm0%40group.calendar.google.com&amp;color=%23125A12&amp;
+							src=zh.hong_kong%23holiday%40group.v.calendar.google.com&amp;color=%23125A12&amp;
+							src=%23contacts%40group.v.calendar.google.com&amp;color=%2328754E&amp;ctz=America%2FNew_York" 
+							target="_blank">My Calendar</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container-fluid -->
+    </nav>
+
+    <!-- Full Page Image Background Carousel Header -->
+    <header id="myCarousel" class="carousel slide">
+        <!-- Indicators -->
+        <ol class="carousel-indicators">
+            <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+            <li data-target="#myCarousel" data-slide-to="1"></li>
+            <li data-target="#myCarousel" data-slide-to="2"></li>
+        </ol>
+
+        <!-- Wrapper for Slides -->
+        <div class="carousel-inner" style="height: 100%;border-color: #F66733;border-bottom-style: solid;border-bottom-width: 55px;">
+            <div class="item active">
+                <!-- Set the first background image using inline CSS below. -->
+				<div class="fill" style="background-image:url('pages/img/clemson.jpg');"></div>
+                <div class="carousel-caption">
+                    <h2>Death Valley</h2>
+                </div>
+            </div>
+            <div class="item">
+                <!-- Set the second background image using inline CSS below. -->
+                 <div class="fill" style="background-image:url('pages/img/shanghai.jpg');"></div>
+                <div class="carousel-caption">
+                    <h2>Shanghai City</h2>
+                </div>
+            </div>
+            <div class="item">
+                <!-- Set the third background image using inline CSS below. -->
+                <div class="fill" style="background-image:url('pages/img/firework.jpg"></div>
+                <div class="carousel-caption">
+                    <h2>Changsha City</h2>
+                </div>
+            </div>
+        </div>
+
+        <!-- Controls -->
+        <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+            <span class="icon-prev"></span>
+        </a>
+        <a class="right carousel-control" href="#myCarousel" data-slide="next">
+            <span class="icon-next"></span>
+        </a>
+
+    </header>
+
+    <!-- Portfolio Grid Section -->
+    <section id="portfolio">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <h2>Courses</h2>
+                    <hr class="star-primary">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-4 portfolio-item">
+                    <a href="#portfolioModal1" class="portfolio-link" data-toggle="modal">
+                        <div class="caption">
+                            <div class="caption-content">
+                                <i class="fa fa-search-plus fa-3x"></i>
+                            </div>
+                        </div>
+                        <img src="pages/img/portfolio/cabin.png" class="img-responsive" alt="">
+                    </a>
+					<p class="portfolio-font">6200 Computer Security Principles</p>
+					
+                </div>
+                <div class="col-sm-4 portfolio-item">
+                    <a href="#portfolioModal2" class="portfolio-link" data-toggle="modal">
+                        <div class="caption">
+                            <div class="caption-content">
+                                <i class="fa fa-search-plus fa-3x"></i>
+                            </div>
+                        </div>
+                        <img src="pages/img/portfolio/cake.png" class="img-responsive" alt="">
+                    </a>
+					<p class="portfolio-font">6620 Database Management Systems</p>
+                </div>
+                <div class="col-sm-4 portfolio-item">
+                    <a href="#portfolioModal3" class="portfolio-link" data-toggle="modal">
+                        <div class="caption">
+                            <div class="caption-content">
+                                <i class="fa fa-search-plus fa-3x"></i>
+                            </div>
+                        </div>
+                        <img src="pages/img/portfolio/circus.png" class="img-responsive" alt="">
+                    </a>
+					<p class="portfolio-font">8700 Software Design</p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- About Section -->
+    <section class="success" id="contact" style="margin-bottom:50px;background:#F66733">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <h2>Contact</h2>
+                    <hr class="star-light" style="background:#F66733">
+                </div>
+            </div>
+   
+			<div class="row" >
+                    <div class="footer-col col-md-4 text-center" ">
+                        <h3>Location</h3>
+                        <p>School of Computing<br>Clemson University</p>
+                    </div>
+					<div class="footer-col col-md-4 text-center" ">
+                        <h3>Email</h3>
+                        <p>yilangl@clemson.edu</p>
+                    </div>
+                    <div class="footer-col col-md-4 text-center">
+                        <h3>Networks</h3>
+                        <ul class="list-inline">
+                            <li>
+                                <a href="https://www.facebook.com/people/Seth-Lee/100012854658156" class="btn-social btn-outline"><i class="fa fa-fw fa-facebook" target="_blank"></i></a>
+                            </li>
+                         
+                            <li>
+                                <a href="https://www.linkedin.com/in/yilang-li-b33309a7" class="btn-social btn-outline"><i class="fa fa-fw fa-linkedin" target="_blank"></i></a>
+                            </li>
+							
+							<li>
+                                <a href="#" class="btn-social btn-outline"><i class="fa fa-fw fa-dribbble"></i></a>
+                            </li>
+                           
+                        </ul>
+                    </div>
+                   
+                </div>
+        </div>
+    </section>
+
+    <!-- Scroll to Top Button (Only visible on small and extra-small screen sizes) -->
+    <div class="scroll-top page-scroll hidden-sm hidden-xs hidden-lg hidden-md">
+        <a class="btn btn-primary" href="#page-top">
+            <i class="fa fa-chevron-up"></i>
+        </a>
+    </div>
+
+
+    <!-- jQuery -->
+    <script src="pages/vendor/jquery/jquery.min.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="pages/vendor/bootstrap/js/bootstrap.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
+
+    <!-- Contact Form JavaScript -->
+    <script src="pages/js/jqBootstrapValidation.js"></script>
+    <script src="pages/js/contact_me.js"></script>
+
+    <!-- Theme JavaScript -->
+    <script src="pages/js/freelancer.min.js"></script>
+	 <!-- Script to Activate the Carousel -->
+    <script>
+    $('.carousel').carousel({
+        interval: 3500//changes the speed
+    })
+    </script>
+
+</body>
+
+</html>
